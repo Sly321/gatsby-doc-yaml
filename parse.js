e5ebb65b3b99c851aa510122d89bee5a6bead0a5
Sven Liebig <liebigsv@gmail.com>
2019-06-28 16:03:41 +0200

    ununsed parse js

--- parse.js
+++
@@ -1,9 +0,0 @@
-const babelParser = require("@babel/parser")
-const fs = require("fs")
-const path = require("path")
-
-const str = fs.readFileSync(
-    path.resolve(__dirname, "library", "Anything.tsx"),
-    "utf8"
-)
-babelParser.parse(str)
